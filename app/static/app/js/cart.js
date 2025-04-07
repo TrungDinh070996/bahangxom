fb1f978e66b697eb1049ab34c2fdb383464c82ee
trungdinh070996 <trungdinh070996@gmail.com>
2025-04-07 12:34:39 +0700

    finished

--- app/static/app/js/cart.js
+++ app/static/app/js/cart.js
@@ -1,52 +1,70 @@
+// function updateUserOrder(productId, action) {
+//   console.log("user logged in");
+//   var url = "/update_item/";
 
-
-function updateUserOrder(productId, action) {
-  console.log("user logged in");
-  var url = "/update_item/";
-
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-CSRFToken": csrftoken,
-    },
-    body: JSON.stringify({ productId: productId, action: action }),
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log("data", data);
-      location.reload();
-    });
-}
+//   fetch(url, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "X-CSRFToken": csrftoken,
+//     },
+//     body: JSON.stringify({ productId: productId, action: action }),
+//   })
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log("data", data);
+//       showToast();
+//       location.reload();
+//     });
+// }
 
 function showToast() {
-  const toastEl = document.getElementById('toast-add-success');
+  const toastEl = document.getElementById("toast-add-success");
   const toast = new bootstrap.Toast(toastEl);
   toast.show();
 }
 
-let updateBtns = document.getElementsByClassName("update-cart");
-
-for (let i = 0; i < updateBtns.length; i++) {
-  updateBtns[i].addEventListener('click', function () {
+let addBtns = document.getElementsByClassName("add-btn");
+for (let i = 0; i < addBtns.length; i++) {
+  addBtns[i].addEventListener("click", function () {
     var productId = this.dataset.product;
     var action = this.dataset.action;
 
-    fetch('/update_item/', {
-      method: 'POST',
+    fetch("/update_item/", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'X-CSRFToken': csrftoken,
+        "Content-Type": "application/json",
+        "X-CSRFToken": csrftoken,
       },
       body: JSON.stringify({ productId: productId, action: action }),
     })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Added:', data);
-      showToast(); // ✅ GỌI TOAST
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        showToast(); // ✅ Chỉ hiện toast ở đây
+      });
   });
 }
 
+
+let updateBtns = document.getElementsByClassName("chg-quantity");
+for (let i = 0; i < updateBtns.length; i++) {
+  updateBtns[i].addEventListener("click", function () {
+    var productId = this.dataset.product;
+    var action = this.dataset.action;
+
+    fetch("/update_item/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": csrftoken,
+      },
+      body: JSON.stringify({ productId: productId, action: action }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        location.reload(); // ✅ Reload lại giỏ hàng
+      });
+  });
+}
