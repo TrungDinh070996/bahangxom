ac8d9f2182e53628852293a169b517e7f781f069
trungdinh070996 <trungdinh070996@gmail.com>
2025-05-16 09:34:38 +0700

    Fix: add DATABASE_URL fallback

--- webproject/settings.py
+++ webproject/settings.py
@@ -80,14 +80,23 @@ WSGI_APPLICATION = 'webproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
-}
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True
+        )
+    }
+else:
+    # fallback dùng SQLite cho local dev
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
